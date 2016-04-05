c250876c9ee8d0b256756b096be39c8cfc540297
destinysync <destinysync@hotmail.com>
2016-04-05 12:23:36 +0000

    demo

--- s.js
+++ s.js
@@ -1,37 +1,12 @@
-'use strict';
-
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-    secret: 'secretClementine',
-    resave: false,
-    saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
 var mongo = require('mongodb').MongoClient;
 var originalURL = '';
 var prefix = "https://url-shortener-service-destinysync.c9users.io/";
-var urlPairs = {};
 var resultJSON = {};
 
 function getResultJSON(originalURL, shortenedURL) {
@@ -144,9 +119,8 @@ app.get('/:serial', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-    res.sendFile('/public/index2.html');
+    res.sendFile(process.cwd() + '/public/index2.html');
 });
-
 
 app.listen(port, function() {
     console.log('Node.js listening on port ' + port + '...');
